0a05f9601cdd83d7a5a8aceacd166fd3026687cb
sidf <flv@mail.com>
2017-03-23 21:17:09 +0200

    API Server: added the NumberInterval class

--- Implementation/API Server/src/fsega/distributedsystems/server/ServerRunner.java
+++ Implementation/API Server/src/fsega/distributedsystems/server/ServerRunner.java
@@ -1,13 +1,17 @@
 package fsega.distributedsystems.server;
 
+import fsega.distributedsystems.server.helpers.NumericInterval;
+
 // http://tutorials.jenkov.com/java-multithreaded-servers/
 public class ServerRunner {
 	public static void main(String[] args) {
 		int portIndex = findArgIndex(args, "--port");
 		int threadsIndex = findArgIndex(args, "--threads");
 		
-		int port = getCommandLineArgumentValue(args, portIndex, 0, 65535, 8080);
-		int threads =  getCommandLineArgumentValue(args, threadsIndex, 1, 64, 32);
+		int port = getCommandLineArgumentValue(args, portIndex, new NumericInterval<Integer>(0, 65535), 8080);
+		int threads =  getCommandLineArgumentValue(args, threadsIndex, new NumericInterval<Integer>(1, 64), 32);
+		
+		System.out.printf("Port: %d%nThread count: %d%n", port, threads);
 		
 		SimpleServer server = SimpleServer.getInstance(port, threads);
 		Thread thread = new Thread(server);
@@ -22,11 +26,11 @@ public class ServerRunner {
 		server.stopServer();
 	}
 	
-	private static int getCommandLineArgumentValue(String[] args, int itemIndex, int minAcceptedValue, int maxAcceptedValue, 
+	private static int getCommandLineArgumentValue(String[] args, int itemIndex, NumericInterval<Integer> interval, 
 												   int defaultValue) {
 		if (itemIndex != -1 && itemIndex + 1 < args.length && args[itemIndex + 1].matches("^\\d+$")) {
 			int value = Integer.valueOf(args[itemIndex + 1]);
-			return value <= maxAcceptedValue && value >= minAcceptedValue ? value : defaultValue;
+			return interval.isInRange(value) ? value : defaultValue;
 		}
 		
 		return defaultValue;
---
+++ Implementation/API Server/src/fsega/distributedsystems/server/helpers/NumericInterval.java
@@ -0,0 +1,16 @@
+package fsega.distributedsystems.server.helpers;
+
+public class NumericInterval <T extends Number> {
+	private T min;
+	private T max;
+	
+	public NumericInterval (T min, T max) {
+		this.min = min;
+		this.max = max;
+	}
+	
+	public boolean isInRange(T number) {
+		double doubleNumber = number.doubleValue();
+		return doubleNumber >= min.doubleValue() && doubleNumber <= max.doubleValue();
+	}
+}
