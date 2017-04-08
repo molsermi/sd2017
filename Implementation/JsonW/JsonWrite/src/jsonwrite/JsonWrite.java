889b37d401396ad93034d94f76d11f01b86b2085
CirtinaFlorin <florin.cirtina@outlook.com>
2017-04-08 21:29:17 +0300

    Implementare Output Writer Json - scriere in fisier extern

--- Implementation/JsonW/JsonWrite/src/jsonwrite/JsonWrite.java
+++ Implementation/JsonW/JsonWrite/src/jsonwrite/JsonWrite.java
@@ -4,20 +4,40 @@
  * and open the template in the editor.
  */
 package jsonwrite;
-
+import org.json.JSONObject;
+import org.json.JSONArray;
+import java.io.IOException;
+import java.io.FileWriter;
+import org.json.JSONException;
 /**
  *
  * @author Florin
  */
 public class JsonWrite {
-    
+    private static final String jsonFilePath="D:\\Master an 1\\Sem 2\\Sisteme distribuite\\SD\\Proiect\\JsonWrite.json";
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JSONException {
         // TODO code application logic here
+        JSONObject jsonObject = new JSONObject();
+        jsonObject .put("SERVICE_NAME:", "S1");
+        jsonObject .put("VALUE:", "3.47");
         
+        
+        
+        
+        try{
+            FileWriter jsonFileWriter = new FileWriter(jsonFilePath);
+            jsonFileWriter.write(jsonObject.toString());
+            jsonFileWriter.flush();
+            jsonFileWriter.close();
+            System.out.println(jsonObject);
+        
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
     
 }
