52c7c816194a0638ca112daa273f4b043459c282
sidf <flv@mail.com>
2017-04-12 21:25:35 +0300

    API Server: added an initial impl. of the OutputBuilder class

--- Implementation/API Server/src/fsega/distributedsystems/server/util/OutputBuilder.java
+++ Implementation/API Server/src/fsega/distributedsystems/server/util/OutputBuilder.java
@@ -1,5 +1,23 @@
 package fsega.distributedsystems.server.util;
 
-public class OutputBuilder {
+import org.json.JSONObject;
 
+import fsega.distributedsystems.server.util.exceptions.NoSuchServiceException;
+
+public class OutputBuilder {
+	// TODO: talk to services, get back objects and turn them into JSON
+	public static String getJsonForUrl(String url) throws Exception {
+		final JSONObject jsonObject = new JSONObject();
+		
+		if (url.equals("S1")) {
+			jsonObject.put("VALUE", "3.14");
+		} else if (url.equals("S2")) {
+			jsonObject.put("VALUE", "14.3");
+		} else{ 
+			throw new NoSuchServiceException(String.format("Cannot provide results for service %s", url));
+		}
+		
+		jsonObject.put("SERVICE_NAME", url);
+		return jsonObject.toString();
+	}
 }
---
+++ Implementation/API Server/src/fsega/distributedsystems/server/util/exceptions/NoSuchServiceException.java
@@ -0,0 +1,7 @@
+package fsega.distributedsystems.server.util.exceptions;
+
+public class NoSuchServiceException extends Exception {
+    public NoSuchServiceException(String message) {
+        super(message);
+    }
+}
